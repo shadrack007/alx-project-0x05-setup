6ff05b9ccba2afa0b3b399b54c566ec381a57f65
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-22 10:52:42 +0300

    initial commit

---
+++ alx-project-0x09/interfaces/index.ts
@@ -0,0 +1,13 @@
+export interface ReactComponentProps {
+  children: React.ReactNode;
+}
+
+export interface GeneratedImageProps {
+  imageUrl: string;
+  prompt: string;
+  width?: string;
+  height?: string;
+  action: (imagePath: string) => void;
+}
+
+export type ImageProps = Pick<GeneratedImageProps, "imageUrl">;
